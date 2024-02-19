d63d3687f41fc984243c8f65af59745f2b8e3d78
Agnik Sarkar <121817949+agniksarkar1107@users.noreply.github.com>
2024-02-19 14:26:34 +0530

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -1,3 +1,4 @@
 import streamlit as st 
 st.write("#  Multiple Disease Predictor WebApp ")
 st.markdown("Welcome to our groundbreaking project on multiple organ failure detection, where cutting-edge machine learning algorithms meet innovative technological stacks to revolutionize healthcare. Our mission is to leverage the power of data science to predict and prevent the onset of multiple organ failure, a critical condition with high mortality rates. Through advanced analytics, real-time monitoring, and interdisciplinary collaboration, we aim to enhance patient outcomes and ultimately save lives. Join us in our journey towards a future where early detection and intervention redefine the landscape of critical care medicine.")
+st.subheader("Made by the TAS Group featuring Agnik,Ambarish,Debanjan.")
